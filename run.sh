c8f492e0603ca27fb275127d4891b20b21772659
Яковлева Виктория <136712378+YakovlevaVictoriia@users.noreply.github.com>
2026-06-12 16:25:17 +0000

    начала работу с run.sh, первые три команды из 1 блока задания

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# https://www.gnu.org/software/bash/manual/bash.html#Case-Conditionals - вот здесь прочитала про то, какой синтаксис у case (можно по поиску на странице достаточно быстро найти)
+
+case "$1" in 
+    build_generator)
+        echo "Собран образ для контейнера генератора"
+        docker build -t generator ./generator
+        ;;
+    run_generator)
+        echo "Запущен контейнер"
+        mkdir -p data
+        docker run --rm -v "$(pwd)/data:/data" generator
+        echo "Контейнер сгенерирован локально: data/data.csv"
+        ;;
+    create_local_data)
+        echo "Запуск генератора для локальной отладки"
+        mkdir -p local_data
+        python3 generator/generate.py local_data
+        echo "Создан local_data/data.csv"
+        ;;
+    *)
+        echo "Использование: ./run.sh {build_generator|run_generator|create_local_data}"
+        exit 1
+        ;;
+esac
